bebccbd99ca5492f965d4e389fb409630b027e1d
unknown <hectorgzz89@gmail.com>
2021-08-16 13:47:54 -0400

    get values of rock when moving, make rock appear again once hits zero

--- script.js
+++ script.js
@@ -7,11 +7,32 @@ function jump() {
     dino.classList.add('jump-animation');
     setTimeout(() => {
         dino.classList.remove('jump-animation');    
-    }, 250);
+    }, 500);
     //remove animation after 250 s
 }
 
 document.addEventListener('keypress', () => {
+    //prevents user from pressing keys while dino in mid air
+    if (!dino.classList.contains('jump-animation')){
     jump();
     //bind function to key
-})
+    }
+});
+
+setInterval(() => {
+    const dinoTop = parseInt(window.getComputedStyle(dino)
+      .getPropertyValue('top'));
+
+    const rockLeft = parseInt(window.getComputedStyle(rock)
+      .getPropertyValue('left'));
+    //shows position of rock while moving across screen
+    if (rockLeft < 0) {
+        rock.style.display = 'none';
+    //if position 0 or negative make rock disapper 
+    } else {
+        rock.style.display = '';
+    //makes rock reapear- loops again
+    }
+   
+
+}, 50);
